1bc0f701e164b4338f6a79cdf46f4aa4d520a0a2
Svetlana Mikisheva <svetamikis@yandex.ru>
2021-10-01 21:28:03 +0500

    обновлен код

---
+++ element-search/popups/task.js
@@ -0,0 +1,33 @@
+document.getElementById('modal_main').className = 'modal modal_active';
+
+let modalClose = document.getElementsByClassName('modal__close modal__close_times');
+let showSuccess = document.getElementsByClassName('show-success').item(0);
+
+
+modalClose.item(0).onclick = () => document.getElementById('modal_main').className = 'modal';
+
+showSuccess.onclick = () => document.getElementById('modal_success').className = 'modal modal_active';
+
+modalClose.item(1).onclick = () => {
+
+	document.getElementById('modal_success').className = 'modal';
+	document.getElementById('modal_main').className = 'modal';
+}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
